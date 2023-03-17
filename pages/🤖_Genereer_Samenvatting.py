32a9b8276c4d5e14c2eb3152e7700539e63cd652
MarcB77 <59608472+MarcB77@users.noreply.github.com>
2023-03-17 15:01:13 +0100

    update info text in nederlands

--- pages/🤖_Genereer_Samenvatting.py
+++ pages/🤖_Genereer_Samenvatting.py
@@ -48,9 +48,9 @@ with st.spinner("Even een samenvatting aan het schrijven, momentje..."):
         st.markdown(spacy_NER_output, unsafe_allow_html=True)
 
 st.info(
-    """Model temperature:\n - Higher values like 0.8 will make the output more 
-    random\n - Lower values like 0.2 will make it more focused and deterministic\n\n 
-    De NER-functie: \n\n Kan entiteiten in ongestructureerde tekst identificeren en categoriseren. 
+    """Model temperature:\n - Hogere waarden zoals 0.8 zal de output meer random 
+    maken\n - Lagere waarden zoals 0.2 zal de output meer gericht en deterministisch maken
+    \n\n De NER-functie: \n\n Kan entiteiten in ongestructureerde tekst identificeren en categoriseren. 
     Bijvoorbeeld: \n- Personen\n - Plaatsen\n - Organisaties\n - Hoeveelheden.""", 
     icon="ℹ️")
 
