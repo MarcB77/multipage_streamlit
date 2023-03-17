0db3db6131c7cddc7f29b365b8d11cd3bf79628d
MarcB77 <59608472+MarcB77@users.noreply.github.com>
2023-03-17 14:42:08 +0100

    logo sidebar

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -8,15 +8,14 @@ st.set_page_config(
     initial_sidebar_state='expanded'
 )
 
-col1, col2 = st.columns(2)
-
 image = Image.open('image/southfields_logo2.png')
 image_aurai = Image.open('image/aurai_logo.png')
 
-with col1:
-    st.image(image)
-with col2:
-    st.image(image_aurai)
+# col1, col2 = st.columns(2)
+# with col1:
+st.image(image)
+# with col2:
+st.sidebar.image(image_aurai)
 
 st.write("# Welcome to South-Fields Demo!")
 
