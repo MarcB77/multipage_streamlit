f023634d2c4d47db4da4f2820568440b48a5b760
MarcB77 <59608472+MarcB77@users.noreply.github.com>
2023-03-23 16:12:17 +0100

    format prompt

--- prompt_engineering/prompt_utils/create_prompt.py
+++ prompt_engineering/prompt_utils/create_prompt.py
@@ -8,32 +8,32 @@ def create_prompt(
     score_dict: dict
 ) -> str:
     prompt = (
-        """Schrijf een wedstrijd samenvatting met minimaal 100 woorden en met maximaal 200 woorden over de tekst.
-            Schrijf in de stijl van een sport verslaggever.
-            tekst: 
-                Wedstrijd: """
-        + home_team
-        + """ speelt thuis tegen """
-        + away_team
-        + """.
-                Events: """
-        + "".join(event_prompt)
-        + """
-                Balbezit: """
-        + home_team
-        + """ Had """
-        + home_team_possession
-        + """ procent balbezit en """
-        + away_team
-        + """ had """
-        + away_team_possession
-        + """ procent balbezit.
-                Eindstand: """
-        + winning_team
-        + """ heeft gewonnen met """
-        + str(score_dict["Goals"][0])
-        + """-"""
-        + str(score_dict["Goals"][1])
-        + """."""
+        """ Schrijf een wedstrijd samenvatting met minimaal 100 woorden en met maximaal 200 woorden over de tekst.
+    Schrijf in de stijl van een sport verslaggever.
+    tekst: 
+    Wedstrijd: """
+    + home_team
+    + """ speelt thuis tegen """
+    + away_team
+    + """.
+    Events: """
+    + "".join(event_prompt)
+    + """
+    Balbezit: """
+    + home_team
+    + """ Had """
+    + home_team_possession
+    + """ procent balbezit en """
+    + away_team
+    + """ had """
+    + away_team_possession
+    + """ procent balbezit.
+    Eindstand: """
+    + winning_team
+    + """ heeft gewonnen met """
+    + str(score_dict["Goals"][0])
+    + """-"""
+    + str(score_dict["Goals"][1])
+    + """."""
     )
     return prompt
