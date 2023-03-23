8ddb66f8cd71cac3b23db783f6b5ac5416e804b3
MarcB77 <59608472+MarcB77@users.noreply.github.com>
2023-03-23 10:23:11 +0100

    include datetime

--- pages/🤖_Genereer_Samenvatting.py
+++ pages/🤖_Genereer_Samenvatting.py
@@ -1,43 +1,69 @@
+import calendar
+import datetime
+import uuid
+
+import pytz
 import spacy
 import streamlit as st
 from PIL import Image
 from streamlit_chat import message as st_message
-import uuid
-import sqlalchemy.orm as _orm
 
+import utils.database_utils.services as _services
 from utils.api_gpt__3_5 import streamlit_prompt, streamlit_prompt_curie
 from utils.entity_dataframe import create_df
-import utils.database_utils.services as _services
 
-if 'message_history' not in st.session_state:
+if "message_history" not in st.session_state:
     st.session_state.message_history = []
+
 
 def create_AWS_DB():
     _services.create_AWS_database()
+
 
 def prompt_to_DB(PROMPT):
     UUID = str(uuid.uuid1())
 
     _services.insert_prompt(UUID=UUID, prompt=PROMPT)
 
+
 @st.cache_data(show_spinner="Een momentje...")
 def load_images():
-    return Image.open('image/southfields_logo.png')
+    return Image.open("image/southfields_logo.png")
+
 
 @st.cache_resource(show_spinner="Een momentje...")
 def load_nl_core_news_lg():
     return spacy.load("nl_core_news_lg")
-     
+
 
 def streamlit_page_config():
-    st.set_page_config(page_title="Genereer Samenvatting", page_icon="🤖", layout='wide', initial_sidebar_state='expanded')
+    st.set_page_config(
+        page_title="Genereer Samenvatting",
+        page_icon="🤖",
+        layout="wide",
+        initial_sidebar_state="expanded",
+    )
     hide_streamlit_style = """
                 <style>
                 #MainMenu {visibility: hidden;}
                 footer {visibility: hidden;}
                 </style>
                 """
-    st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+    st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+
+def get_datetime() -> str:
+    created_at = datetime.datetime.now(pytz.timezone("Europe/Amsterdam"))
+    created_at_formatted = (
+        str(calendar.month_name[created_at.month])
+        + " "
+        + str(created_at.day)
+        + ": "
+        + str(created_at.strftime("%H:%M:%S"))
+        + "\n\n"
+    )
+    return created_at_formatted
+
 
 streamlit_page_config()
 st.sidebar.success("Genereer een samenvatting op deze demo pagina.")
@@ -49,49 +75,30 @@ st.image(image)
 st.write(""" # South-Fields Demo """)
 
 temperature_GPT = st.sidebar.number_input(
-    label="Model Temperature",
-    min_value=0.0,
-    max_value=1.0,
-    value=0.5
-    )
+    label="Model Temperature", min_value=0.0, max_value=1.0, value=0.5
+)
 
 input_data = st.text_area(
-    label="Wedstrijd Data",
-    value="..",
-    height=200,
-    max_chars=None
-    )
+    label="Wedstrijd Data", value="..", height=200, max_chars=None
+)
 submit = st.button("Genereer")
 
 if submit:
     with st.spinner("Even een samenvatting aan het schrijven, momentje..."):
         if input_data != "..":
-            
             generated_output = streamlit_prompt_curie(input_data, TEMP=temperature_GPT)
+            _datetime = get_datetime()
+            st.session_state.message_history.append(_datetime + generated_output)
+            for message_ in st.session_state.message_history:
+                st_message(
+                    message_, avatar_style="bottts-neutral", seed="Aneka", is_user=False
+                )
 
-            st.session_state.message_history.append(generated_output)
-            for message_ in  st.session_state.message_history:
-            
-                st_message(message_,
-                        avatar_style="bottts-neutral",
-                        seed="Aneka",
-                        is_user=False)
-            
-            
-            #spacy_NER_output = spacy.displacy.render(NER(generated_output),style="ent",jupyter=False)
-            # st.markdown("""# Named Entity Recognition """)
-            # st.markdown(spacy_NER_output, unsafe_allow_html=True)
             with st.spinner("Wegschrijven naar database"):
                 prompt_to_DB(generated_output)
 
 st.info(
-        """Model temperature:\n - Hogere waarden zoals 0.8 zal de output meer random 
-        maken\n - Lagere waarden zoals 0.2 zal de output meer gericht en deterministisch maken""", 
-        icon="ℹ️")
-    # st.info(
-    #         """Model temperature:\n - Hogere waarden zoals 0.8 zal de output meer random 
-    #         maken\n - Lagere waarden zoals 0.2 zal de output meer gericht en deterministisch maken
-    #         \n\n De NER-functie: \n\n Kan entiteiten in ongestructureerde tekst identificeren en categoriseren.
-    #         \n Entiteiten lijst: \n""", 
-    #         icon="ℹ️")
-    #st.dataframe(data=create_df(), width=550)
+    """Model temperature:\n - Hogere waarden zoals 0.8 zal de output meer random 
+        maken\n - Lagere waarden zoals 0.2 zal de output meer gericht en deterministisch maken""",
+    icon="ℹ️",
+)
