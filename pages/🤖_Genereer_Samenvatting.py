65c2af48dbf8660fa410b2eb27a13f1c6af0d94b
MarcB77 <59608472+MarcB77@users.noreply.github.com>
2023-03-24 13:13:09 +0100

    login page

--- pages/🤖_Genereer_Samenvatting.py
+++ pages/🤖_Genereer_Samenvatting.py
@@ -11,6 +11,7 @@ from streamlit_chat import message as st_message
 import utils.database_utils.services as _services
 from utils.api_gpt__3_5 import streamlit_prompt, streamlit_prompt_curie
 from utils.entity_dataframe import create_df
+from utils.st_login import check_password
 
 if "message_history" not in st.session_state:
     st.session_state.message_history = []
@@ -63,41 +64,41 @@ def get_datetime() -> str:
     )
     return created_at_formatted
 
+if check_password():
+    streamlit_page_config()
+    st.sidebar.success("Genereer een samenvatting op deze demo pagina.")
+    create_AWS_DB()
+    NER = load_nl_core_news_lg()
+    image = load_images()
+    st.image(image)
 
-streamlit_page_config()
-st.sidebar.success("Genereer een samenvatting op deze demo pagina.")
-create_AWS_DB()
-NER = load_nl_core_news_lg()
-image = load_images()
-st.image(image)
+    st.write(""" # South-Fields Demo """)
 
-st.write(""" # South-Fields Demo """)
+    temperature_GPT = st.sidebar.number_input(
+        label="Model Temperature", min_value=0.0, max_value=1.0, value=0.5
+    )
 
-temperature_GPT = st.sidebar.number_input(
-    label="Model Temperature", min_value=0.0, max_value=1.0, value=0.5
-)
+    input_data = st.text_area(
+        label="Wedstrijd Data", value="..", height=200, max_chars=None
+    )
+    submit = st.button("Genereer")
 
-input_data = st.text_area(
-    label="Wedstrijd Data", value="..", height=200, max_chars=None
-)
-submit = st.button("Genereer")
+    if submit:
+        with st.spinner("Even een samenvatting aan het schrijven, momentje..."):
+            if input_data != "..":
+                generated_output = streamlit_prompt_curie(input_data, TEMP=temperature_GPT)
+                _datetime = get_datetime()
+                st.session_state.message_history.append(_datetime + generated_output)
+                for message_ in reversed(st.session_state.message_history):
+                    st_message(
+                        message_, avatar_style="bottts-neutral", seed="Aneka", is_user=False
+                    )
 
-if submit:
-    with st.spinner("Even een samenvatting aan het schrijven, momentje..."):
-        if input_data != "..":
-            generated_output = streamlit_prompt_curie(input_data, TEMP=temperature_GPT)
-            _datetime = get_datetime()
-            st.session_state.message_history.append(_datetime + generated_output)
-            for message_ in reversed(st.session_state.message_history):
-                st_message(
-                    message_, avatar_style="bottts-neutral", seed="Aneka", is_user=False
-                )
+                with st.spinner("Wegschrijven naar database"):
+                    prompt_to_DB(generated_output)
 
-            with st.spinner("Wegschrijven naar database"):
-                prompt_to_DB(generated_output)
-
-st.info(
-    """Model temperature:\n - Hogere waarden zoals 0.8 zal de output meer random 
-        maken\n - Lagere waarden zoals 0.2 zal de output meer gericht en deterministisch maken""",
-    icon="ℹ️",
-)
+    st.info(
+        """Model temperature:\n - Hogere waarden zoals 0.8 zal de output meer random 
+            maken\n - Lagere waarden zoals 0.2 zal de output meer gericht en deterministisch maken""",
+        icon="ℹ️",
+    )
