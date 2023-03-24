d229e02698ab8b44c89c681c7c9c84031a75f2d0
MarcB77 <59608472+MarcB77@users.noreply.github.com>
2023-03-24 13:07:49 +0100

    Test login page

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -24,22 +24,51 @@ def load_images():
     image_aurai = Image.open('image/aurai_logo.png')
     return image, image_aurai
 
-streamlit_page_config()
-image, image_aurai = load_images()
-# col1, col2 = st.columns(2)
-# with col1:
-st.image(image)
-# with col2:
+def check_password():
+    """Returns `True` if the user had the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if st.session_state["password"] == st.secrets["password"]:
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # don't store password
+        else:
+            st.session_state["password_correct"] = False
+
+    if "password_correct" not in st.session_state:
+        # First run, show input for password.
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        return False
+    elif not st.session_state["password_correct"]:
+        # Password not correct, show input + error.
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        st.error("😕 Password incorrect")
+        return False
+    else:
+        # Password correct.
+        return True
+
+if check_password():
+    streamlit_page_config()
+    image, image_aurai = load_images()
+    # col1, col2 = st.columns(2)
+    # with col1:
+    st.image(image)
+    # with col2:
 
 
-st.write("# Welcome to South-Fields Demo!")
+    st.write("# Welcome to South-Fields Demo!")
 
-st.sidebar.success("Selecteer een demo hierboven.")
-st.sidebar.image(image_aurai)
+    st.sidebar.success("Selecteer een demo hierboven.")
+    st.sidebar.image(image_aurai)
 
-st.markdown(
+    st.markdown(
+        """
+        Author: Marc Blomvliet (Aurai) \n
+        **👈 Selecteer een demo uit de sidebar** 
     """
-    Author: Marc Blomvliet (Aurai) \n
-    **👈 Selecteer een demo uit de sidebar** 
-"""
-)
+    )
